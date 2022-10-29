0824ab034a8e93d2e3315940ce198b4809387151
stanleywangQA <swang40@eq.edu.au>
2022-10-30 08:46:44 +1000

    a

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotArm.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp()
+public class RobotArm extends OpMode{
+
+    Servo claw = null;
+    DcMotor roboArm = null;
+
+    public void init() {
+        claw = hardwareMap.get(Servo.class, "Servo");
+        roboArm = hardwareMap.get(DcMotor.class, "Motor");
+    }
+
+
+    public void loop() {
+        double MIN_POSITION = 0.0;
+        double MAX_POSITION = 1.0;
+
+        if (claw.getDirection() == Servo.Direction.FORWARD) {
+            //double clawPos = claw.getDirection();
+        } else if (claw.getDirection() == Servo.Direction.REVERSE) {
+
+        }
+
+        if (gamepad1.right_bumper) {
+             claw.setDirection(Servo.Direction.FORWARD);
+        }
+        if (gamepad1.left_bumper) {
+            claw.setDirection(Servo.Direction.REVERSE);
+        }
+        roboArm.setPower(gamepad1.left_stick_y);
+    }
+}
